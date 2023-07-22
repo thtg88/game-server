29bed38e7ffb69f17e46129ee2796bb28f74fdbb
Marco Marassi <16927402+thtg88@users.noreply.github.com>
2023-07-22 14:56:13 +0100

    Test Keep-Alive settings

--- internal/remoteserver/grpcserver.go
+++ internal/remoteserver/grpcserver.go
@@ -8,8 +8,10 @@ import (
 	"io"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 type GrpcRandomGameServer struct {
@@ -35,7 +37,20 @@ func (rrgs *GrpcRandomGameServer) Serve() error {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
 
-	server := grpc.NewServer()
+	kaep := keepalive.EnforcementPolicy{
+		MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+		PermitWithoutStream: true,            // Allow pings even when there are no active streams
+	}
+
+	kasp := keepalive.ServerParameters{
+		MaxConnectionIdle:     20 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+		MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+		MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+		Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+		Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+	}
+
+	server := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
 	pb.RegisterGameServer(server, rrgs)
 
 	log.Printf("server listening at %v", lis.Addr())
