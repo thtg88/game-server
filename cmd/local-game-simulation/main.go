51874b5262173f8f8aa69f8bae49c2198249200b
Marco Marassi <16927402+thtg88@users.noreply.github.com>
2023-07-24 19:06:59 +0100

    Handle shutdown signals for local game simulation

--- cmd/local-game-simulation/main.go
+++ cmd/local-game-simulation/main.go
@@ -3,11 +3,41 @@ package main
 import (
 	"game-server/internal/client"
 	"game-server/internal/server"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	rgs := server.New()
 
 	rgs.Loop()
-	client.Spawn(rgs)
+	go client.Spawn(rgs)
+
+	shutdownCh := handleShutdownSignal(func() {
+		rgs.Shutdown()
+	})
+
+	<-shutdownCh
+
+	log.Println("[local-game-simulation] shutting down gracefully")
+}
+
+func handleShutdownSignal(onShutdownReceived func()) chan struct{} {
+	shutdownCh := make(chan struct{})
+
+	go func() {
+		sigNotifier := make(chan os.Signal, 1)
+
+		signal.Notify(sigNotifier, os.Interrupt, syscall.SIGTERM)
+
+		// Park here until a signal is received
+		<-sigNotifier
+
+		onShutdownReceived()
+		close(shutdownCh)
+	}()
+
+	return shutdownCh
 }
